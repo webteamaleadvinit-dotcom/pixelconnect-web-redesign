37c46c5b2d3822b825b0e5369fd6888bfb42fc45
user.name like: "Himanshu Swaroop <user.email like : "himanshuswrp21@gmail.com>
2025-08-25 09:38:05 +0530

    added why choose us

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../Components/Hero";
 import WhatWeDo from "../Components/WhatWeDo";
 import JoinTeam from "../Components/JoinTeam";
+import WhyChooseUs from "../Components/WhyChooseUs";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <WhatWeDo />
+      <WhyChooseUs />
       <JoinTeam />
     </div>
   );
